cc662ce09ad2220bd357a470a4428c4538188ce6
kulshekhar <kulshekhar@users.noreply.github.com>
2017-12-05 20:47:53 +0530

    introduce error

--- e2e/test.js
+++ e2e/test.js
@@ -2,6 +2,6 @@ describe('Default test', () => {
   it('runs successfully', async () => {
     browser.url('/');
     expect(browser.getTitle()).toBe('page title');
-    expect(browser.getText('body')).toContain('placeholder');
+    expect(browser.getText('body')).toContain('placeholders');
   });
 });
